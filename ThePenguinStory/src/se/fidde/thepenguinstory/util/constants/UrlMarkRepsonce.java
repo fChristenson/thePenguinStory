ed9566bb21a5969da7d0e9ea99c7ada1bd55bed6
fidde <fredrik.christenson@gmail.com>
2014-04-14 08:17:30 +0200

    removed old src dir

--- ThePenguinStory/src/se/fidde/thepenguinstory/util/constants/UrlMarkRepsonce.java
+++
@@ -1,6 +0,0 @@
-package se.fidde.thepenguinstory.util.constants;
-
-public enum UrlMarkRepsonce {
-
-    IN_COLLECTION, FOUND, ERROR;
-}
--- ThePenguinStory/src/se/fidde/thepenguinstory/util/validation/HttpUrlPrefixes.java
+++
@@ -1,5 +0,0 @@
-package se.fidde.thepenguinstory.util.validation;
-
-public enum HttpUrlPrefixes {
-    WWW_PREFIX, HTTP_WWW_PREFIX, INVALID_FORMAT, NO_PREFIX;
-}
